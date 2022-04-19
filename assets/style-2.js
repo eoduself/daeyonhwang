0114cb1d947b417e2cdc3daefb561e7e318906b3
DAE YON HWANG <44008869+eoduself@users.noreply.github.com>
2022-04-18 23:17:47 -0400

    Update style-2.js

--- assets/style-2.js
+++ assets/style-2.js
@@ -14,7 +14,7 @@ const highlightcolor = "#111";
 // Body properties
 const bodyfontweight = 300;
 const bodyfontsize = "12pt";
-const backgroundcolor = "#799496"; //#fff
+const backgroundcolor = "#fff"; //#fff
 
 // Link properties
 const acolor = accentcolor;
